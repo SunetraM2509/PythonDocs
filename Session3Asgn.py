9771572949b9f7f883f388068c56c594f0ca5b29
Sunetra Mukherjee <66210592+SunetraM2509@users.noreply.github.com>
2020-10-10 18:50:18 +0530

    Uploaded assignment 3
    Assignment 3 contains basic python programs for printing statements, changing cases & quotes.

---
+++ Session3Asgn.py
@@ -0,0 +1,24 @@
+##Personal Message: Store a person’s name in a variable, and print a messageto that person.
+
+message = "Hello Sunetra, would you like to learn some Python today?"
+print(message)
+
+#Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+person_name = "sunetra mukherjee"
+print(person_name)
+print(person_name.title())
+print(person_name.upper())
+
+#Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+
+quote = Malcolm Forbes once said, "Failure is success If we learn from it."
+print(quote)
+
+#Famous Quote 2: Repeat Exercise 3-3, but this time store the famous person’s name in a variable called famous_person. 
+#Then compose your message and store it in a new variable called message. Print your message.
+
+famous_person = "Malcolm Forbes"
+message = once said, "Failure is success If we learn from it."
+quote = famous_person + " " + message
+print(quote)
