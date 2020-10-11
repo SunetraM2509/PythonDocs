d0e9fd4b69df92cc437b15d07cd40e0953b92fdd
Sunetra Mukherjee <66210592+SunetraM2509@users.noreply.github.com>
2020-10-11 19:11:00 +0530

    Update Session3Asgn.py

--- Session3Asgn.py
+++ Session3Asgn.py
@@ -12,13 +12,13 @@ print(person_name.upper())
 
 #Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
 
-quote = Malcolm Forbes once said, "Failure is success If we learn from it."
+quote = "Malcolm Forbes once said, 'Failure is success If we learn from it.'"
 print(quote)
 
-#Famous Quote 2: Repeat Exercise 3-3, but this time store the famous person’s name in a variable called famous_person. 
+#Famous Quote 2: Repeat Exercise above, but this time store the famous person’s name in a variable called famous_person. 
 #Then compose your message and store it in a new variable called message. Print your message.
 
 famous_person = "Malcolm Forbes"
-message = once said, "Failure is success If we learn from it."
+message = "once said, 'Failure is success If we learn from it.'"
 quote = famous_person + " " + message
 print(quote)
